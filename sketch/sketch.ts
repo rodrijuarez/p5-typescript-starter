e6f2f150c85c5e816f42ee17e035ec8145628c25
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-04-18 20:59:07 +0200

    Small fixes

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,24 +1,18 @@
 var sketch = (p: p5) => {
+  p.preload = () => {};
 
-    const morph = new Morph();
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
 
-    p.preload = () => {
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 
-    }
-    
-    p.setup = () => {
-        p.createCanvas(p.windowWidth, p.windowHeight);
-        morph.setup(p);
-    }
-    
-    p.windowResized = () => {
-        p.resizeCanvas(p.windowWidth, p.windowHeight);
-    }
-    
-    p.draw = () => {
-        p.background(100);
-        morph.draw(p);        
-    }
-}
+  p.draw = () => {
+    p.background(200);
+    p.print(p.frameCount);
+  };
+};
 
 var sketchP = new p5(sketch);
